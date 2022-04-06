bbcdced1d0a61016fad286f92aa84363edc38af2
keita <nameneko820@i.softbank.jp>
2022-04-06 18:53:30 +0900

    init

--- template-parts/property-details/slider.php
+++ template-parts/property-details/slider.php
@@ -3,9 +3,10 @@
 <div class="p-property-details-slider">
     <?php
         for ($i = 0; $i < 10; $i++) {
-            $path = $_SERVER['DOCUMENT_ROOT']."/fujimoto5/web/images/$args[type]/t/".sprintf("%08d",$args['seq'])."-".$i.".jpg";
+            $path = $_SERVER['DOCUMENT_ROOT']."/fujimoto5/web/images/$args[type]/t/".sprintf("%08d",$args['seq'])."-".sprintf("%02d",$i).".jpg";
             echo $path;
-            echo "<img src='$path'>";
+            $img_path = getSystemImgPath($args['type'],$args['seq'],sprintf("%08d",$args['seq'])."-".sprintf("%02d",$i).".jpg");
+            echo "<img src=''>";
             if(file_exists($path)) {
                 echo 'true';
                 //echo "<a class='p-property-details-slider__item' href=''><img class='p-property-details-slider__img' src=''></a>";
